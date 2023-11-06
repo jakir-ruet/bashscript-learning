370da6404e848c0ab23ad18a1a51afea3a34a31e
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-06 14:57:49 +0600

    shell done

--- 02-variables-shell-expansions/02-variables.sh
+++ 02-variables-shell-expansions/02-variables.sh
@@ -16,5 +16,8 @@ echo "Your name is $my_name"
 # such as 
 # PATH variable > that contain list of directories where the shell search for executable files to run as command names. check it by echo $PATH
 # HOME variable > this is used to store the absolute path to the current user's home directory. check it by echo $HOME
-# USER variable > that contains the username of the current 
+# USER variable > that contains the username of the current user. check it by echo $PATH or echo "Hello, $USER"
+# HOSTNAME > that contains the name of current computer. check by echo "$HOSTNAME"
+# HOSTTYPE > that tell us you what type of processor architecture the computer is running. check by echo "$HOSTTYPE"
+# PS1 (Prompt String 1) > that contains the prompt string shown in the terminal before each command. its environmental variable. check by echo "$PS1"
 # 2. Bash Shell Variables (Around 95 in total variable)
