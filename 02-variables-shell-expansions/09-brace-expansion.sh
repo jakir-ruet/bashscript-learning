006d503fc514691f5179bdf4bed504eb6e6cf166
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-08 15:00:51 +0600

    brace expansion done

---
+++ 02-variables-shell-expansions/09-brace-expansion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo {mango, banana, apple}
+echo { mango, banana, apple }
+echo {1...20}
+echo {1..20}
+
+echo {20...1}
+echo {20..1}
+
+echo {a...z}
+echo {a..z}
+echo {1..1000..3} # increment by 3
+
+# cleate file and directories
+mkdir dirName{1..5} 
+touch dirName{1..5} 
