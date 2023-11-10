6ac4c06061c38fdef68ea2dae3dbeca0b34dff0e
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-10 21:04:27 +0600

    positional parameter done

--- 02-variables-shell-expansions/10-positional-parameter.sh
+++ 02-variables-shell-expansions/10-positional-parameter.sh
@@ -4,3 +4,9 @@ father_name=$2
 mother_name=$3
 
 echo $name $father_name $mother_name
+
+# another example we try if linux terminal
+# echo "I am $1"
+# echo "My profession is $2"
+# echo "I live in $3"
+# run ./10-positional-parameter.sh Jakir Enginner $PATH
