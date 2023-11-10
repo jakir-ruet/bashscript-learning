e7190fe4a1329ba600cb1fa80a2c67de1b3c4b09
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-10 21:49:34 +0600

    special parameter done

---
+++ 02-variables-shell-expansions/11-special-parameter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Special parameters ($#, $0) are like regular parameters, but are created for us by the shell, and are unmodifiable.
+# in linux terminal
+# echo "I am $1"
+# echo "My profession is $2"
+# echo "I live in $3"
+# echo $#
+# run ./10-positional-parameter.sh Jakir Enginner $HOME
+ 
