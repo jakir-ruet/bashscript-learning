8d6462e59bc74309e0b90dbc078f40e0681dfb42
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-11-15 14:58:52 +0600

    alias and default version setup

--- 01-intoduction/03-path-add.sh
+++ 01-intoduction/03-path-add.sh
@@ -11,3 +11,12 @@
 # finally, we run the following command
 # echo $PATH
 # Have a good day, Path is added.
+
+# Alias add in mac
+# vi ~/.bash_profile
+# alias python='python3'
+# source ~/.bash_profile
+
+# You can solve it by symbolic link.
+# unlink /usr/local/bin/python
+# ln -s /usr/local/bin/python3.3 /usr/local/bin/python
